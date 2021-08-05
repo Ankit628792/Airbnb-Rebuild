1e6b5d41a922a76b566bed1006ba78c640697af4
Ankit Kumar <ankit628792@gmail.com>
2021-08-05 20:12:31 +0530

    day2

---
+++ components/Footer.jsx
@@ -0,0 +1,51 @@
+function Footer() {
+    return (
+        <>
+        <div className=" bg-white text-gray-600">
+
+            <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-y-10 py-10 px-10 sm:px-16">
+                <div className="space-y-3 text-gray-800">
+                    <h5 className="font-bold">ABOUT</h5>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">How Airbnb Works</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Newsroom</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Airbnb 2021</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Investors</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Airbnb Plus</p>
+                </div>
+                <div className="space-y-3 text-gray-800">
+                    <h5 className="font-bold">COMMUNITY</h5>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Diversity & Belonging</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Accessibility</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Airbnb Associates</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Frontline Stays</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Guest Referrals</p>
+                </div>
+                <div className="space-y-3 text-gray-800">
+                    <h5 className="font-bold">HOST</h5>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Host your home</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Host an Online Experience</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Responsible hosting</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Resource Centre</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Community Centre</p>
+                </div>
+                <div className="space-y-3 text-gray-800">
+                    <h5 className="font-bold">SUPPORT</h5>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Our COVID-19 Response</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Help Centre</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Cancellation options</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Neighbourhood Support</p>
+                    <p className="cursor-pointer hover:text-gray-500 hover:scale-105 transition duration-150 ease-in-out">Trust & Safety</p>
+                </div>
+
+            </div>
+            <hr />
+            <p className="text-center text-xl p-5 font-semibold">
+                Design & Developed by <a href="https://instagram.com/ankit_628792" target="_blank" rel="noreferrer" className="text-red-400">Ankit</a> <br className="sm:hidden" /> &copy; 2021 All right reserved
+            </p>
+
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ['links.papareact.com', 'sample-api-data.vercel.app', 'images.unsplash.com']
+    }
+}
